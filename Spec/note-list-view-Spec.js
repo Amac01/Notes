704baea09c4200df61afc6edd0c3ee2157580472
Alpha Mackie <alphamackie@Alphas-MacBook-Pro.local>
2018-12-12 15:37:36 +0000

    Created a note list model that has no notes, one note or several notes.

--- Spec/note-list-view-Spec.js
+++ Spec/note-list-view-Spec.js
@@ -5,3 +5,15 @@ noteList.addNote("Favourite food: pesto");
 noteListView = new NoteListView(noteList);
 
 testOne.compare(noteListView.returnHTML(), "<ul><li><div>Favourite food: pesto</div></li></ul>");
+
+noteList = new NoteList();
+noteListView = new NoteListView(noteList);
+testOne.compare(noteListView.returnHTML(), "");
+
+noteList = new NoteList();
+noteList.addNote("Favourite food: pesto");
+noteList.addNote("My string");
+noteList.addNote("One More string");
+noteListView = new NoteListView(noteList);
+testOne.compare(noteListView.returnHTML(),
+  "<ul><li><div>Favourite food: pesto</div></li><li><div>My string</div></li><li><div>One More string</div></li></ul>");
--- note-list-view.js
+++ note-list-view.js
@@ -5,7 +5,16 @@
   }
 
   NoteListView.prototype.returnHTML = function() {
-    return "<ul><li><div>" + this.noteList.notes[0].text + "</div></li></ul>";
+    if(this.noteList.notes.length == 0){
+      return "";
+    } else {
+      var result = "<ul>";
+      for(var i = 0; i < this.noteList.notes.length; i++){
+        result += "<li><div>" + this.noteList.notes[i].text + "</div></li>";
+      }
+      return result + "</ul>";
+    }
+
 
   }
   exports.NoteListView = NoteListView;
