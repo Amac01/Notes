03f19b6a6d3b1cc71ab036f272ead3878446ef33
Alpha Mackie <alphamackie@Alphas-MacBook-Pro.local>
2018-12-13 12:07:54 +0000

    Created a note controller file, used a script to load it and the message on the html page.

---
+++ note-controller.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// var myElement = document.querySelector("#app");
+var myElement = document.querySelector("#app").innerHTML = "Howdy";
+// myElement = "Howdy";
+console.log(myElement);
